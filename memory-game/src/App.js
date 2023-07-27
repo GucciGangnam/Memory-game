be15fe570b41acebb3149a1e8f28b139eec48957
Gucci Gangnam <diviner02hive@icloud.com>
2023-07-27 15:22:45 +0700

    game finsihed.  all functions working and styles added

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -24,7 +24,8 @@ function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
   const [firstChoice, setfirstChoice] = useState(null)
-  const [secondChoice, setSecondChoice] = useState(null)
+  const [secondChoice, setsecondChoice] = useState(null)
+  const [lowestScore, setlowestScore] = useState('No record set')
 
 
 
@@ -35,7 +36,7 @@ const createNewDeck = () => {
   setCards([])
   setTurns(0)
   setfirstChoice(null)
-  setSecondChoice(null)
+  setsecondChoice(null)
   //create 12 cards
   const newDeck = [...cardImages, ...cardImages]
   // shuffle the cards
@@ -48,7 +49,7 @@ const createNewDeck = () => {
 } 
 
 const MakeChoice = (theCard) => { 
-  // if first card is trur and thecardID is = to first card ID then return
+  // if first card is trur and thecardID is = to first card ID then retur
   if (firstChoice !== null && (theCard.id === firstChoice.id)) { 
     console.log('cant click same card twice')
     return
@@ -56,7 +57,7 @@ const MakeChoice = (theCard) => {
   if (firstChoice === null) { 
     setfirstChoice(theCard)
   } else if (secondChoice === null) { 
-    setSecondChoice(theCard)
+    setsecondChoice(theCard)
   } else { 
     console.log('both choices already made')
   }
@@ -66,36 +67,55 @@ const MakeChoice = (theCard) => {
   //   chosenCard.classList.add('Chosen')
 };
 
-useEffect(() => {
-  if (firstChoice && secondChoice){
-    runMatchChecker(); 
-    //flip cards
-  }
-}, [firstChoice, secondChoice]);
+  useEffect(() => {
+    if (firstChoice && secondChoice) {
+      runMatchChecker();
+    }
+  }, [firstChoice, secondChoice]);
 
 // check if matches are legit 
-const runMatchChecker = () => { 
-  
-    if (firstChoice.src === secondChoice.src) { 
-      setCards(poopoo => { 
-        return poopoo.map(card =>{ 
-          if (card.src === firstChoice.src){ 
-            return {...card, matched: true}
-          } else { 
+  const runMatchChecker = () => {
+    if (firstChoice.src === secondChoice.src) {
+      setCards(poopoo => {
+        return poopoo.map(card => {
+          if (card.src === firstChoice.src) {
+            return { ...card, matched: true }
+          } else {
             return card
           }
         })
       })
       setTurns(turns + 1)
-    } else { 
-
+    } else {
       setTurns(turns + 1)
     }
+    setTimeout(() => setfirstChoice(null), 500)
+    setTimeout(() => setsecondChoice(null), 500)
+  }
 
-    setfirstChoice(null)
-    setSecondChoice(null)
+  useEffect(() => {
+    // Check if all 'matched' properties are true, but also check if 'cards' is not empty and is an array
+    const allMatchesAreFound = Array.isArray(cards) && cards.length > 0 && cards.every(card => card.matched === true);
   
-}
+    // Log the current 'cards' array
+    console.log(cards);
+  
+    // If all matches are found, log the message 'gameOver - all matches found!'
+    if (allMatchesAreFound) { 
+      updateLowestScore()
+    }
+  }, [cards, setCards]);
+  
+  const updateLowestScore = () => { 
+    if (lowestScore === 'No record set'){ 
+      setlowestScore(turns)
+    }
+    if (turns < lowestScore) { 
+      setlowestScore(turns)
+    }
+
+
+  }
 
 
   return (
@@ -116,13 +136,14 @@ const runMatchChecker = () => {
           card={card} 
           key={card.id}  
           MakeChoice={MakeChoice}
+          flipped={card === firstChoice || card === secondChoice || card.matched}
           />
         ))}
       </div>
 
     <div className="Score-Div">
       <div className="Score-Text">Current Score: {turns}</div>
-      <div className="High-Score-Text">Lowest Score: 0</div>
+      <div className="High-Score-Text">Lowest Score: {lowestScore}</div>
     </div>
 
 
--- memory-game/src/components/singleCard.js
+++ memory-game/src/components/singleCard.js
@@ -3,34 +3,27 @@ import { useState } from "react"
 
 
 
-export const SingleCard = ({card, MakeChoice}) => {
+export const SingleCard = ({card, MakeChoice, flipped}) => {
 
 
 const cardClickHandler = () => {
     MakeChoice(card)
-    //flip the card
-
-    
-
-
-
 }
 
     return (
         <div className='Card' id={card.id}>
-            <div>
+            <div className={flipped ? 'flipped' : ''}>
                 <img 
                 id="frontOfCard"
                 className='CardFront' 
                 src={card.src} alt='Card Front' 
-                style={{width: '100%', height: '100%'}} />
+                />
 
                 <img 
                 id="BackOfCard"
                 className='CardBack' 
                 src='/img/Card-back.png' 
                 alt='Card back' 
-                style={{width: '100%', height: '100%' }}
                 onClick={cardClickHandler}
                 />
             </div>
